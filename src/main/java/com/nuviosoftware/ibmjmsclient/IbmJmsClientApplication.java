8a5aa21b2de7f943f7ca428a1d666d36facd8752
riccardo <riccardonoviello@futuregov.com>
2020-05-16 15:39:19 +0100

    add initial project files

---
+++ src/main/java/com/nuviosoftware/ibmjmsclient/IbmJmsClientApplication.java
@@ -0,0 +1,13 @@
+package com.nuviosoftware.ibmjmsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmJmsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmJmsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuviosoftware/ibmjmsclient/IbmJmsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuviosoftware.ibmjmsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmJmsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
